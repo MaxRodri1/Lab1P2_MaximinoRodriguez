66fdc66c4696c0f70fd14d93988ebb5b8dd55393
MaxRodri1 <rodriguezmaximino2005@gmail.com>
2024-04-19 15:29:12 -0600

    Laboratorio 1 commit 4

--- Lab1P2_MaximinoRodriguez/src/lab1p2_maximinorodriguez/Lab1P2_MaximinoRodriguez.java
+++ Lab1P2_MaximinoRodriguez/src/lab1p2_maximinorodriguez/Lab1P2_MaximinoRodriguez.java
@@ -4,6 +4,7 @@
  */
 package lab1p2_maximinorodriguez;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,15 +18,15 @@ public class Lab1P2_MaximinoRodriguez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        //Fila 2 Asiento 6
 
+        //Fila 2 Asiento 6
         System.out.println("");
         System.out.println("Ejercicio Practico Mediana de Medianas");
         System.out.println("");
 
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
+        ArrayList Mediana = new ArrayList();
 
         System.out.println("Bienvenido al ejercicio de Mediana de Medianas");
         System.out.println("Ingrese el tamano deseado para la matriz,que sea un numero impar y mayor que 4");
@@ -50,26 +51,79 @@ public class Lab1P2_MaximinoRodriguez {
                         MatrizRandom[ContadorI][ContadorJ] = NumeroAleatorio;
                     }
                 }
-
+                System.out.println("");
+                System.out.println("Matriz Randomizada");
+                System.out.println("");
+                
                 for (int ContadorI = 0; ContadorI < MatrizRandom.length; ContadorI++) {
                     for (int ContadorJ = 0; ContadorJ < MatrizRandom[ContadorI].length; ContadorJ++) {
                         System.out.print("[" + MatrizRandom[ContadorI][ContadorJ] + "]");
                     }
                     System.out.println("");
                 }
+
+                System.out.println("");
+
+                int[] Arreglo = new int[TamanoMatriz];
+
+                for (int i = 0; i < TamanoMatriz; i++) {
+                    Arreglo[i] = MatrizRandom[0][i];
+                }
+
+                for (int i = 0; i < Arreglo.length; i++) {
+                    System.out.print("[" + Arreglo[i] + "]");
+                }
+
+                for (int ContadorJ = 0; ContadorJ < 3; ContadorJ++) {
+                    for (int i = 0; i < Arreglo.length - 1; i++) {
+                        if (Arreglo[i] > Arreglo[i + 1]) {
+                            int CambiadorX = Arreglo[i];
+                            int CambiadorY = Arreglo[i + 1];
+                            Arreglo[i] = CambiadorY;
+                            Arreglo[i + 1] = CambiadorX;
+                        }
+                    }
+                }
                 
+                System.out.println("");
+                
+                for (int i = 0; i < Arreglo.length; i++) {
+                    System.out.print("[" + Arreglo[i] + "]");
+                }
+                
+                System.out.println("");
+                System.out.println("");
+                
+                System.out.println("Matriz Ordenada");
+                for (int i = 0; i < 3; i++) {
+                    for (int ContadorFilas = 0; ContadorFilas < MatrizRandom.length; ContadorFilas++) {
+                        for (int ContadorColumnas = 0; ContadorColumnas < MatrizRandom[ContadorFilas].length - 1; ContadorColumnas++) {
+                            if (MatrizRandom[ContadorFilas][ContadorColumnas] > MatrizRandom[ContadorFilas][ContadorColumnas + 1]) {
+                                int CambiadorX = MatrizRandom[ContadorFilas][ContadorColumnas];
+                                int CambiadorY = MatrizRandom[ContadorFilas][ContadorColumnas+1];
+                                MatrizRandom[ContadorFilas][ContadorColumnas]= CambiadorY;
+                                MatrizRandom[ContadorFilas][ContadorColumnas+1] = CambiadorX;
+                            }
+
+                        }
+                    }
+
+                }
+                
+                for (int ContadorI = 0; ContadorI < MatrizRandom.length; ContadorI++) {
+                    for (int ContadorJ = 0; ContadorJ < MatrizRandom[ContadorI].length; ContadorJ++) {
+                        System.out.print("[" + MatrizRandom[ContadorI][ContadorJ] + "]");
+                    }
+                    System.out.println("");
+                }
+
+                System.out.println("");
+
                 
 
             }
         }
 
-    }
-    
-    
-    public static int BubbleSort(int[][]MatrizRandom){
-        
-        
-        return 0;
     }
 
 }
